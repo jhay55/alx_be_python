0da03faf47cc71937abcb825cee1631123c2507e
jhay55 <asajhay40@gmail.com>
2024-09-26 13:34:01 -0700

    added basic operations file

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+#This is going to be my first python script file
+
+# Defining the predetermined variables
+number1 = 10
+number2 = 5
+
+# Here are some basic operations performed on the two variables
+sum_of_numbers = number1 + number2
+diff_of_numbers = number1 -number2
+prod_of_numbers = number2*number1
+
+#onto the print commands
+print('assuming the number1 = ' +str(number1)+ ' and the number2= '+str(number2))
+print('Addition of ' +str(number1) +' and ' +str(number2) +' is ' + str(sum_of_numbers))
+print('Subtraction of ' +str(number1) +' and ' +str(number2) +' is ' + str(diff_of_numbers))
+print('Multiplication of ' +str(number1) +' and ' +str(number2) +' is ' + str(prod_of_numbers))
