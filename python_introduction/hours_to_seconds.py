15f605d073f5e7923f235e912c89a28c5a24566d
jhay55 <asajhay40@gmail.com>
2024-09-27 16:08:51 -0700

    added hours to seconds file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+#this file executes a script by converting hours to seconds
+
+hours = 2
+seconds = hours * 3600
+
+print('Suppose there are 3600 second(s) in an hour,...' )
+print(str(hours)+ ' hour(s) is '+str(seconds)+ ' seconds')
